a194e36b4cb398b65f9ffde76d2333eee29d7010
Akahad1 <aksahad234567@gmail.com>
2024-09-22 10:19:30 +0600

    add name

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import "./App.css";
+import Navber from "./pages/Home/Navber/Navber";
 
 function App() {
   return (
     <>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+      <Navber></Navber>
     </>
   );
 }
---
+++ src/pages/Home/Navber/Navber.tsx
@@ -0,0 +1,13 @@
+const Navber = () => {
+  const Bio = {
+    name: "sahad",
+    age: 34,
+  };
+  return (
+    <div>
+      <h2>{Bio.name}</h2>
+    </div>
+  );
+};
+
+export default Navber;
